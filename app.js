e93a32611bbe6708708d1204010fa026f76f506b
Vlad <sergatyi.vlad@rambler.ru>
2018-07-09 20:36:21 +0300

    Up project

--- app.js
+++ app.js
@@ -5,7 +5,13 @@ var url = 'mongodb://localhost:27017/example';
 MongoClient.connect(url).then(function (client) { // <- db as first argument
     const db = client.db('Loog');
     const collection = db.collection('users');
+    db.collection("users").deleteMany({login: "Vlados"}, function(err, result){
+
+
+
+   });
     let userOne = {
+
       login: 'Vlados',
       password: '123456'
     };
@@ -14,12 +20,37 @@ MongoClient.connect(url).then(function (client) { // <- db as first argument
         return console.log(err);
       }
       app.post('/login2', urlencodedParser, function (req, res) {
-        if (req.body.login == result.ops[0].login && req.body.password == result.ops[0].password) {
-          res.send('Вы вошли на сайт');
-        } else {
-          res.sendFile(__dirname + '/static/index.html');
-        }
+        collection.find().toArray(function(err, resultis){
+          for (var i = 0; i < resultis.length; i++){
+          if (req.body.login == resultis[i].login && req.body.password == resultis[i].password) {
+            res.send('Вы вошли на сайт');
+          } else {
+            res.sendFile(__dirname + '/static/index.html');
+          }
+          }
+        console.log(resultis);
       });
+
+      });
+      app.post('/regis', urlencodedParser, function (req, res) {
+        if (req.body.password3 == req.body.password4) {
+          collection.find().toArray(function(err, results){
+
+          console.log(results.length);
+
+          var use = {
+
+          }
+          use.login = req.body.login3;
+          use.password = req.body.password3;
+          collection.insertOne(use, function (err, resul) {
+            console.log(resul.ops);
+            res.sendFile(__dirname + '/static/index.html');
+          })
+
+      });
+        }
+      })
     })
   });
 var app = express();
