b1ef182b06ffc927abb718153a12d0f73c3d8d61
Vlad <sergatyi.vlad@rambler.ru>
2018-07-13 14:39:57 +0300

    Up

--- app.js
+++ app.js
@@ -1,64 +1,27 @@
-const express = require('express');
-const bodyParser = require('body-parser');
+const express        = require('express');
 const MongoClient    = require('mongodb').MongoClient;
-var url = 'mongodb://localhost:27017/example';
-MongoClient.connect(url).then(function (client) { // <- db as first argument
-    const db = client.db('Loog');
-    const collection = db.collection('users');
-    db.collection("users").deleteMany({login: "Vlados"}, function(err, result){
-
-
-
-   });
-    let userOne = {
-
-      login: 'Vlados',
-      password: '123456'
-    };
-    collection.insertOne(userOne, function (err, result) {
-      if (err) {
-        return console.log(err);
-      }
-      app.post('/login2', urlencodedParser, function (req, res) {
-        collection.find().toArray(function(err, resultis){
-          for (var i = 0; i < resultis.length; i++){
-          if (req.body.login == resultis[i].login && req.body.password == resultis[i].password) {
-            res.send('Вы вошли на сайт');
-          } else {
-            res.sendFile(__dirname + '/static/index.html');
-          }
-          }
-        console.log(resultis);
-      });
-
-      });
-      app.post('/regis', urlencodedParser, function (req, res) {
-        if (req.body.password3 == req.body.password4) {
-          collection.find().toArray(function(err, results){
-
-          console.log(results.length);
-
-          var use = {
-
-          }
-          use.login = req.body.login3;
-          use.password = req.body.password3;
-          collection.insertOne(use, function (err, resul) {
-            console.log(resul.ops);
-            res.sendFile(__dirname + '/static/index.html');
-          })
-
-      });
-        }
-      })
-    })
+const bodyParser     = require('body-parser');
+const db             = require('./config/db');
+const app            = express();
+const port = 3000;
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.static('public'));
+MongoClient.connect(db.url, (err, database) => {
+  var db = database.db('notess');
+  const user = {
+    login: 'admin',
+    password: 123456
+  }
+  db.collection('users').removeMany({login: 'admin'}, (err, resultis) =>{
+    if (err) {return console.log(err);}
+  })
+  db.collection('users').insertOne(user, (err, results) => {
+    if (err) {return console.log(err);}
+    console.log(results.ops);
   });
-var app = express();
-var urlencodedParser = bodyParser.urlencoded({extended: false});
-app.use('/', express.static('static'));
-app.get('/', function (req, res) {
-  res.sendFile(__dirname + '/static/index.html');
-});
-app.listen(3000, '127.0.0.1', function () {
-  console.log('Server listening port 3000');
-});
+  if (err) return console.log(err)
+  require('./app/routes')(app, db);
+  app.listen(port, () => {
+    console.log('We are live on ' + port);
+  });
+})
---
+++ app/routes/note_routes.js
@@ -0,0 +1,28 @@
+module.exports = function(app, db) {
+  app.post('/notes', (req, res) => {
+    const note = { text: req.body.body, title: req.body.title };
+    db.collection('notes').insert(note, (err, result) => {
+      if (err) {
+        res.send({ 'error': 'An error has occurred' });
+      } else {
+        res.send(result.ops[0]);
+      }
+    });
+  });
+  app.post('/login', (req, res) => {
+
+    db.collection('users').find().toArray((err, resul) => {
+      if (err) {return console.log(err);}
+      console.log(resul[0].login);
+      if (req.body.login == resul[0].login){
+        res.send('Вы прошли авторизацию');
+      }
+      else {
+        res.sendFile(__dirname + '/public/index.html');
+      }
+    })
+  });
+  app.get('/', (req, res) => {
+    res.sendFile('index.html');
+  })
+};
