947ad13237c35962b2ca1cc31518b20d9460d56f
Serhiy Arendarchuk <mazahell@users.noreply.github.com>
2018-07-15 02:00:12 +0300

    Revert "tasks (#5)"
    This reverts commit 64f97609ed42ea305d5a483db6ced4dc8373c02d.

--- app/routes/note_routes.js
+++ app/routes/note_routes.js
@@ -1,4 +1,3 @@
-// ізбавитись від app,db , заюзать http://expressjs.com/ru/4x/api.html#router
 module.exports = function(app, db) {
   app.post('/notes', (req, res) => {
     const note = { text: req.body.body, title: req.body.title };
@@ -10,42 +9,23 @@ module.exports = function(app, db) {
       }
     });
   });
-
   app.post('/login', (req, res) => {
-    const { login, password } = req.body;
-
-    // написать валідацію вхідних даних
-    // логін - мін 3 символа, пароль мінімум 6, всі поля обовязкові
-    // https://github.com/skaterdav85/validatorjs
-    // або https://validatejs.org/
-
     db.collection('users')
-      .find() // тільки пошук по логіну ({login: login})
-      // пошук лише одного запису (limit: 1)
-      .toArray((err, result) => {
+      .find()
+      .toArray((err, resul) => {
         if (err) {
-          // повернути помилку
           return console.log(err);
         }
-        // прибрати []
-        if (login === result[0].login && password === String(result[0].password)) {
-          // req.session.username = result[0]._id; // primary idx
-          req.session.user = result[0]._id;
-          // редірект на /cabinet
-          res.json({ 1: 2 });
-          // res.send('Вы прошли авторизацию');
+        console.log(resul[0].login);
+        if (req.body.login == resul[0].login && req.body.password == resul[0].password) {
+          req.session.authorized = true;
+          req.session.username = request.body.login;
+          res.send('Вы прошли авторизацию');
         } else {
-          // заюзать .render + 
           res.sendFile(process.env.PWD + '/public/index.html');
         }
       });
   });
-
-  app.get('/cabinet', (req, res) => {
-    res.json({ sess: req.session.user });
-  });
-
-  // розібратись  з індексною сторінкою
   app.get('/', (req, res) => {
     res.sendFile('index.html');
   });
