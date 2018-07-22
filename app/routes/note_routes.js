5567d0359b892c45f3055307fef98c58dca0152a
VladSergatyi <40866312+VladSergatyi@users.noreply.github.com>
2018-07-22 15:39:57 +0300

    request (#9)

--- app/routes/note_routes.js
+++ app/routes/note_routes.js
@@ -1,92 +1,90 @@
 // ізбавитись від app,db , заюзать http://expressjs.com/ru/4x/api.html#router
 'use strict';
-
+const user = require('./../../config/mongoose');
 const Validator = require('./../libs/Validator');
 
 module.exports = app => {
+  const objectDataPage = {
+    title: '',
+    errors: []
+  };
+  // LOGIN Block
+  app.get('/login', (req, res) => {
+    res.render('pages/login', {title:"Log In", errors:[]});
+  });
   app.post('/login', (req, res) => {
     const { login, password } = req.body;
 
     const validate = new Validator(
       { login, password },
       {
-        login: 'required|in:admin',
-        password: 'required|in:123456'
+        login: 'required|min:3',
+        password: 'required|min:6'
+      }
+    );
+    validate.fails(function() {
+      objectDataPage.title = 'Log In';
+      objectDataPage.errors = 'Введите корректные данные.';
+      res.render('pages/login', objectDataPage);
+    });
+    validate.passes( () => {
+      user.find({login:login, password: password}).count((err, count) => {
+        if (err) next(err);
+        if (count > 0) {
+          res.send('Welcome');
+        }
+        else
+        {
+          objectDataPage.title = 'Log In';
+          objectDataPage.errors = 'Данные введены неверно!';
+          res.render('pages/login', objectDataPage);
+        }
+      });
+    });
+  });
+// SIGNUP Block
+  app.get('/signup', (req, res) => {
+      res.render('pages/signup', {title:"Sign Up", errors:[]});
+  });
+  app.post('/signup', (req, res, next) => {
+    const { login, password, password2 } = req.body;
+    const validate = new Validator(
+      { login, password, password2 },
+      {
+        login: 'required|min:3',
+        password: 'required|min:6|same:password2',
+        password2:'required|min:6'
       }
     );
 
-    const objectDataPage = {
-      title: 'Login page',
-      errors: []
-    };
-
-    validate.fails(function() {
-      objectDataPage.title = 'Error';
-      objectDataPage.errors = validate.errors.all();
-      res.render('pages/auth/login', objectDataPage);
+    validate.fails(function(err) {
+      objectDataPage.title = 'Sign Up';
+      objectDataPage.errors = 'Введите корректные данные.';
+      res.render('pages/signup', objectDataPage);
     });
-
     validate.passes(function() {
-      res.render('pages/auth/login', objectDataPage);
-    });
-  });
-
-  app.get('/logup', (req, res) => {
-    res.render('pages/logup', {
-      title: 'Log Up',
-      error: 0
-    });
-  });
-  app.post('/logup', (req, res, next) => {
-    let j = 0;
-    const { login, password, password2 } = req.body;
-
     // пошук по логіну в бд (count)
-    user.find().count(function(err, count) {
-      throw new Error('user exist');
-
+    user.find({login:login}).count(function(err, count) {
       if (err) next(err);
-      if (
-        // переписать під валідатор
-        validator.isLength(login, { min: 3, max: undefined }) == true &&
-        validator.isLength(password, { min: 6, max: undefined }) == true &&
-        validator.isLength(password2, { min: 6, max: undefined }) == true &&
-        password === password2
-      ) {
-        users.forEach(userNumber => {
-          if (login == userNumber.login) {
-            console.log(123);
-            j++;
-            res.render('pages/logup', {
-              title: 'Log Up',
-              error: 1,
-              errorContent: 'Пользователь с таким логилом уже зарегистрирован!'
-            });
-          }
+      if (count == 0) {
+        let person = new user({
+          login: login,
+          password: password
         });
-        if (j == 0) {
-          var person = new user({
-            login: login,
-            password: password
-          });
-          person.save(err => {
-            if (err) throw err;
-          });
-          console.log(person);
-          res.send('ok');
-        }
-      } else {
-        res.render('pages/logup', {
-          title: 'Log Up',
-          error: 0
+        person.save(err => {
+          if (err) next(err);
         });
+        res.send('Поздравляю! Пользователь зарегистрирован!');
+      }
+      else {
+        objectDataPage.title = 'Sign Up';
+        objectDataPage.errors = 'Такой логин уже зарегистрирован!';
+        res.render('pages/signup', objectDataPage);
       }
     });
   });
-  // розібратись  з індексною сторінкою
-  app.get('/', (req, res) => {
-    res.render('pages/home', {
-      title: 'Home page'
-    });
   });
+  app.use((err, req, res, next) => {
+    res.sendStatus(404);
+  })
 };
